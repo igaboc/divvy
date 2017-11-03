270c8fb5e80039ce4270a8615e3df09f3487530d
igaboc <isabellegaboc@hotmail.com>
2017-11-03 11:46:23 +1100

    Add rental fee and deposit to listings form edit and show page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -85,6 +85,6 @@ class ListingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
-      params.require(:listing).permit(:title, :description, :owner_id, :return_time, :category)
+      params.require(:listing).permit(:title, :description, :owner_id, :return_time, :category, :rental_fee, :deposit)
     end
 end
