4d29d6fb176b050340a6fad1d6c2dd9956a9a020
igaboc <isabellegaboc@hotmail.com>
2017-11-03 16:08:47 +1100

    Add Conversations Feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :profiles
   get 'home/index'
   root :to => 'home#index'
+  resources :conversations do
+    resources :messages
+  end
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
