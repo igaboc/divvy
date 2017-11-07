f01bcce7c5db4cb01e37bc5880ea1f84dae47a54
igaboc <isabellegaboc@hotmail.com>
2017-11-07 14:04:22 +1100

    Add styling to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @listings = Listing
+    @listing_five = Listing.find(5)
+    @listing_nine = Listing.find(9)
+    @listing_eight = Listing.find(8)    
   end
 end
